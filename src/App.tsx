03f285b37f621f8a2fab57ccc818730fe1ca597f
Siddharth Rakesh <siddharth.rakesh@rubrik.com>
2020-04-24 02:01:17 +0530

    Upgrading to typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { About } from './About';
 import './App.css';
@@ -13,7 +13,6 @@ const App = () => {
           <Route path="/" component={About} exact />
           <Route path="/resume" component={Resume} exact />
           <Route path="/projects" component={Projects} exact />
-          <Route component={Error} />
         </Switch>
       </div>
     </BrowserRouter>
