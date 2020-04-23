c545f66003c76f1ab4aa9462be4551aa6d983383
Siddharth Rakesh <siddharth.rakesh@rubrik.com>
2020-04-23 23:21:29 +0530

    Remove react app stuff

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { About } from './About';
 import './App.css';
+import { Projects } from './Projects';
+import { Resume } from './Resume';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div>
+        <Switch>
+          <Route path="/" component={About} exact />
+          <Route path="/resume" component={Resume} exact />
+          <Route path="/projects" component={Projects} exact />
+          <Route component={Error} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
