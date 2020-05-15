0a62f3d8a9b0efc526c3d3dd98a11e21985f3c57
Siddharth Rakesh <siddharth.rakesh@rubrik.com>
2020-05-15 22:28:26 +0530

    Update colors to use 6 digit hex

--- src/ColorUtils.tsx
+++ src/ColorUtils.tsx
@@ -2,6 +2,6 @@
 /* For blue theme, use: #89ABE3FF and white */
 /* Ref: https://www.designwizard.com/blog/design-trends/colour-combination */
 
-export const lightGreen: string = '#A8D5BAFF';
-export const lightestSky: string = '#E7EBE0FF';
-export const clearWater: string = '#ABD6DFFF';
+export const lightGreen: string = '#A8D5BA';
+export const lightestSky: string = '#E7EBE0';
+export const clearWater: string = '#ABD6DF';
