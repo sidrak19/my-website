4a9b92cf8ba37ebcec9c1229f380d8eab5b5bb4c
Siddharth Rakesh <siddharth.rakesh@rubrik.com>
2020-05-23 16:39:30 +0530

    Make white back more translucent

--- src/LayoutUtils.tsx
+++ src/LayoutUtils.tsx
@@ -7,7 +7,7 @@ export const Page = styled.div({
   flexDirection: 'column',
   flex: '1 1 auto',
   marginBottom: '0',
-  backgroundColor: 'rgba(255, 255, 255, 0.9)',
+  backgroundColor: 'rgba(255, 255, 255, 0.85)',
   padding: '32px',
   '@media (max-width: 768px)': {
     padding: '16px',
