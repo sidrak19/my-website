7b7f17127b8e886ff8258b727a6f14bc4eccc01d
Siddharth Rakesh <siddharth.rakesh@rubrik.com>
2020-06-05 18:15:36 +0530

    Update favicon

--- src/Home/Quotes/FavoriteQuotes.tsx
+++ src/Home/Quotes/FavoriteQuotes.tsx
@@ -1,20 +1,29 @@
 import * as React from 'react';
 import { Section, SpacedItemDiv } from '../../LayoutUtils';
-import { StyledH2Center } from '../../TextUtils';
+import { StyledH2Center, ExternalLink } from '../../TextUtils';
 
-export const FavoriteQuotes: React.FC = () => {
-  const quotes = [
-    {
-      quote: `The place where you lock yourself in and lock all else out - that's
+interface IQuote {
+  quote: string;
+  attr: string;
+  url: string;
+}
+
+const quotes: Array<IQuote> = [
+  {
+    quote: `The place where you lock yourself in and lock all else out - that's
           not your home. Your home is sometimes a place you travel long and far
           to find.`,
-      attr: 'Geralt of Rivia',
-    },
-    {
-      quote: `Saruman believes it is only great power that can hold evil in check, but that is not what I have found. I found it is the small everyday deeds of ordinary folk that keep the darkness at bay. Small acts of kindness and love.`,
-      attr: 'Gandalf',
-    },
-  ];
+    attr: 'Geralt of Rivia',
+    url: 'https://witcher.fandom.com/wiki/Geralt_of_Rivia',
+  },
+  {
+    quote: `Saruman believes it is only great power that can hold evil in check, but that is not what I have found. I found it is the small everyday deeds of ordinary folk that keep the darkness at bay. Small acts of kindness and love.`,
+    attr: 'Gandalf the Grey',
+    url: 'https://lotr.fandom.com/wiki/Gandalf',
+  },
+];
+
+export const FavoriteQuotes: React.FC = () => {
   return (
     <Section>
       <StyledH2Center>Favorite Quotes</StyledH2Center>
@@ -22,7 +31,8 @@ export const FavoriteQuotes: React.FC = () => {
         return (
           <SpacedItemDiv key={index}>
             <cite>"{quote.quote}"</cite>
-            {` - ${quote.attr}`}
+            {' - '}
+            <ExternalLink link={quote.url} text={quote.attr} />
           </SpacedItemDiv>
         );
       })}
