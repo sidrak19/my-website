d43e887b1deb4a7d7ff6e843492f0af76e1985cf
Siddharth Rakesh <siddharth.rakesh@rubrik.com>
2021-05-17 13:40:46 +0530

    adding

--- src/Projects/InnerChild/InnerChild.tsx
+++ src/Projects/InnerChild/InnerChild.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { CodepenProject } from '../CodepenProject';
-import { Line, ExternalLink } from '../../TextUtils';
+import { Line } from '../../TextUtils';
 
 export const InnerChild: React.FC = () => {
   return (
@@ -10,14 +10,16 @@ export const InnerChild: React.FC = () => {
       description={
         <>
           <Line>
-            Inspired from the sculpture at the{' '}
-            <ExternalLink link="shorturl.at/cuCK9" text="Burning man festival in Nevada" />
-            , which demonstrates a conflict between a man and a woman as well as
-            the outer and inner expression of human nature. This interactive SVG uses{' '}
-            <code>clip-path</code> for showing selective pieces of an SVG on mouse
-            interaction.
+            Inspired from the sculpture at the Burning man festival in Nevada, which
+            demonstrates a conflict between a man and a woman as well as the
+            outer and inner expression of human nature. This interactive SVG
+            uses <code>clip-path</code> for showing selective pieces of an SVG
+            on mouse interaction.
           </Line>
-          <Line>Hover over or touch the image of the couple to reveal their inner selves.</Line>
+          <Line>
+            Hover over or touch the image of the couple to reveal their inner
+            selves.
+          </Line>
         </>
       }
     />
--- src/index.tsx
+++ src/index.tsx
@@ -8,10 +8,7 @@ import './index.css';
 import * as serviceWorker from './serviceWorker';
 // import { render } from 'react-snapshot';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
