40d642345642d61b9c5d74c2d016bf9254e83ff9
Siddharth Rakesh <siddharth.rakesh@rubrik.com>
2022-04-02 15:38:28 +0530

    Upgrade react to v18

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,16 @@ import 'react-app-polyfill/ie11';
 import 'react-app-polyfill/stable';
 
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import {createRoot} from 'react-dom/client';
 import App from './App';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const container = document.getElementById('root');
+if (!container) throw new Error('Failed to find the root element');
+
+const root = createRoot(container);
+root.render(<App />);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
